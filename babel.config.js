e33d96388f85a2049b3fe28327edc33522d9f331
sergioDvMX <52550555+sergioDvMX@users.noreply.github.com>
2024-07-27 16:29:54 -0600

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = api => {
   api.cache(true);
 
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:@react-native/babel-preset'],
     plugins: [['react-native-reanimated/plugin']],
     env: {
       production: {
